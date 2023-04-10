6725730cb12bceb0e95c5af5cd755a4fa8a177d2
Martin Conradsen <martinconradsenop@gmail.com>
2023-04-10 11:15:14 +0200

    Changed fire detection and fixed some bugs

--- cannon_auto_control.py
+++ cannon_auto_control.py
@@ -3,31 +3,51 @@ from fire_detector import *
 import vonage
 from read_ir_sensors import *
 import time
+import sys
 
 ports = serial.tools.list_ports.comports()
 for port in ports:
     print(str(port))
-serialInst = serial.Serial('/dev/cu.usbmodem141201')
+serialInst = serial.Serial('/dev/ttyACM0')
 serialInst.baudrate = 9600
 
 client = vonage.Client(key="79267e59", secret="Master23")
 sms = vonage.Sms(client)
 
 fire_detected = False
+count = 0
+forward = True
 
 def extinguish_fire():
+    global fire_detected, count, forward
+    serialInst.write('m'.encode('utf-8'))
+    time.sleep(0.2)
+    serialInst.write('h'.encode('utf-8'))
+    time.sleep(0.2)
+    serialInst.write('wwwwwwww'.encode('utf-8'))
     configure_cv2()
     while (not fire_detected):
         if (camera_found_fire()):
             print("Camera found fire.")
-            if (ir_fire_detected()):
+            #if (ir_fire_detected()):
+            if (True):
                 print("IR sensors detected fire.")
                 print("Firing projectile.")
                 fire_detected = True
                 serialInst.write('f'.encode('utf-8'))
+                time.sleep(1)
+                serialInst.write('j'.encode('utf-8'))
+                sys.exit()
                 break
-
-        serialInst.write('aaaaaa'.encode('utf-8'))
+        if (count < 20 and forward == True):
+            serialInst.write('aaaaaa'.encode('utf-8'))
+            count += 1
+        else:
+            forward = False
+            serialInst.write('dddddd'.encode('utf-8'))
+            count -= 1
+        if (count == -1):
+            forward = True
     #send_text_alert()
 
 def send_text_alert():
